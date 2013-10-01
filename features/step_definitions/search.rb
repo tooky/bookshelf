99af79e71483c4da4fe43804a8752830f29b10c1
Chris Wilson <chris.wilson@onthebeach.co.uk>
2013-10-01 14:27:29 +0100

    Extract Library class

--- features/step_definitions/search.rb
+++ features/step_definitions/search.rb
@@ -1,16 +1,21 @@
 Given(/^there are no books with the title I want$/) do
-  @library = ["alice in wonderland","toad hall","agile development for pros"]
   @searchterm = "Coocumber"
 end
 
 When(/^I search for a book with that title$/) do
-  @results = search(@searchterm)
+  @results = Library.new.search(@searchterm)
 end
 
 Then(/^there should be no results$/) do
   expect(@results).to have_exactly(0).items
 end
 
-def search(searchterm)
-  @library.find_all { |book| book == searchterm }
+class Library
+  def search(searchterm)
+    collection.find_all { |book| book == searchterm }
+  end
+
+  def collection
+    ["alice in wonderland","toad hall","agile development for pros"]
+  end
 end
