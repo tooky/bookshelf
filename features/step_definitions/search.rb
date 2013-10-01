958b785a84218a3847e51dd7a0f521a141124d28
Chris Wilson <chris.wilson@onthebeach.co.uk>
2013-10-01 15:40:07 +0100

    Merch duplicate step definition files
    Two sets of step definitions had been created by teams working on the
    same feature separate from each other. This commit merges the duplicate
    files and deletes the spare.

--- features/step_definitions/search.rb
+++ features/step_definitions/search.rb
@@ -1,5 +1,3 @@
-require_relative '../../library'
-
 Given(/^there are no books with the title I want$/) do
   @searchterm = "Coocumber"
 end
@@ -10,6 +8,19 @@ end
 
 Then(/^there should be no results$/) do
   expect(results).to have_exactly(0).items
+end
+
+Given(/^there are multiple books with similar titles$/) do
+  initialize_bookshelf
+end
+
+When(/^I search for a book title$/) do
+  search_for_title("Cucumber")
+end
+
+Then(/^I see multiple results$/) do
+  expect( @results ).to have_at_least(2).books
+  expect_all_results_to_match("Cucumber")
 end
 
 World(UIDriver)
