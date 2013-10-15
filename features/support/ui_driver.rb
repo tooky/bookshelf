7ec5601fde6a4ee4886252e3ed11984f5d3e9a71
Chris Wilson <chris.wilson@onthebeach.co.uk>
2013-10-15 15:51:44 +0100

    Add WebUIDriver to cucumber support

--- features/support/ui_driver.rb
+++ features/support/ui_driver.rb
@@ -1,6 +1,31 @@
 require 'library'
 
-module UIDriver
+module WebUIDriver
+  def search_by_title(title)
+    visit '/'
+    fill_in 'query', with: title
+    click_button 'submit'
+  end
+
+  def add_book(book)
+    library.add_book(book)
+  end
+
+  def search_results
+    all('.result').map do |element|
+      element.text
+    end
+  end
+
+  def library
+    @library ||= Library.new
+    Sinatra::Application.set :library, @library
+    @library
+  end
+
+end
+
+module DomainDriver
 
   def library
     @library ||= Library.new
@@ -20,5 +45,14 @@ module UIDriver
 
 end
 
-World(UIDriver)
+if ENV['DOMAIN']
+  World(DomainDriver)
+else
+  require 'capybara/cucumber'
+
+  Capybara.app = Sinatra::Application
+  Capybara.app.set :environment, :test
+  Capybara.save_and_open_page_path = File.expand_path("./tmp/capybara")
+  World(WebUIDriver)
+end
 
