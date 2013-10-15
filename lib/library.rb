36b10a55532e0933704b51e2ecb0b5586719ad8a
Chris Wilson <chris.wilson@onthebeach.co.uk>
2013-10-15 15:52:10 +0100

    Add simple search form

--- lib/library.rb
+++ lib/library.rb
@@ -12,5 +12,28 @@ class Library
     raise ArgumentError if title.to_s.strip.length < 1
     @books.find_all { |n| n.match(title) }
   end
+end
 
+require 'sinatra'
+configure do
+  library = Library.new
+  set :library, library
+end
+
+get '/' do
+  %{<html>
+    <head></head>
+    <body>
+      <form action="/search" method="get">
+        <input type="text" id="query" name="query" />
+        <input type="submit" id="submit" value="Search" />
+      </form>
+    </body>
+    </html>
+  }
+end
+
+get '/search' do
+  @results = settings.library.search_by_title( params[:query] )
+  erb :search_results
 end
