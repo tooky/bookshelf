430798ea309b07806cf94f3bf4f74a5af1bf6267
Ali Iybar <ali.iybar@gmail.com>
2013-10-15 15:55:30 +0100

    bookshelf project
    with errors

--- features/support/ui_driver.rb
+++ features/support/ui_driver.rb
@@ -1,6 +1,7 @@
 require 'library'
 
-module UIDriver
+#module UIDriver
+module DomainDriver
 
   def library
     @library ||= Library.new
@@ -20,5 +21,40 @@ module UIDriver
 
 end
 
-World(UIDriver)
+module WebUIDriver
+ def search_by_title(title)
+    visit '/'
+   
+    fill_in 'query', with: title
+    click_button 'Search'
+
+  end
+
+  def search_results 
+          save_and_open_page
+
+     all("li").collect { |item| item.text }
+
+  end
+
+  def add_book(title)
+    #library.add_book title
+    visit '/add_book'
+    fill_in 'book_name', with: title
+    click_button 'Save'
+
+  end 
+end
+
+if ENV['DOMAIN']
+  World(DomainDriver)
+else
+  require 'capybara/cucumber'
+  Capybara.app = Sinatra::Application
+  Capybara.app.set :environment, :test
+  Capybara.save_and_open_page_path = File.expand_path("./tmp/capybara")
+  World(WebUIDriver)
+end 
+
+#World(UIDriver)
 
--- lib/library.rb
+++ lib/library.rb
@@ -11,6 +11,60 @@ class Library
   def search_by_title(title)
     raise ArgumentError if title.to_s.strip.length < 1
     @books.find_all { |n| n.match(title) }
+
   end
 
+end
+
+class WebLibrary
+  def self.library
+      @library ||= Library.new
+  end
+end
+
+require 'sinatra'
+get '/'  do
+  %{<html>
+
+      <body>
+        <form method="get" action="/search">
+          <input type="text" id="query" name="query"/>
+          <input type="submit" id="Search">
+        </form>
+      </body>
+  </html>
+  }
+end
+
+get '/search' do
+  query = params['query']
+#puts query.inspect
+
+  
+  results = WebLibrary.library.search_by_title(query)
+
+
+  head = %{<html><body><ul id="search_results">}
+  list_items = results.map { |r_item| "<li>#{r_item}</li>" } 
+  footer = %{</ul></body></html>}
+
+  [head, list_items, footer].flatten
+end
+
+get '/add_book'  do
+  book_to_save = params['book_name']
+  if !book_to_save.nil?
+    WebLibrary.library.add_book(book_to_save)
+  end
+
+  %{<html>
+
+      <body>
+        <form method="get" action="/add_book">
+          <input type="text" id="book_name" name="book_name"/>
+          <input type="submit" id="Save" name"Save">
+        </form>
+      </body>
+  </html>
+  }
 end
