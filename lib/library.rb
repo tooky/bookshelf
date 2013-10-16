53efe2d181a0d84c87b6acb1a4fa4f1e9d718b94
Joshua Sassel <jdks@ml1.net>
2013-10-16 11:25:48 +0100

    Use ERB templates

--- lib/library.rb
+++ lib/library.rb
@@ -14,33 +14,3 @@ class Library
   end
 
 end
-
-require 'sinatra'
-configure do
-  set :library, Library.new
-end
-
-get '/' do 
-  %{
-<html>
-<head></head>
-<body>
-  <form method="POST">
-    <input type="text" id="query" name="booktitle" />
-
-	   <button id="search">Search</button>
-
-  </form>
-</body>
-</html>
-}
-end
-
-post '/' do
-  books = settings.library.search_by_title(params[:booktitle])
-  return_str = "<ul>"
-  books.each do |book|
-    return_str += "<li class=\"result\">#{book}</li>"
-  end
-  return_str + "</ul>"
-end
